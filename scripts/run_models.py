8e9ff1d96e3cb3c82e8af0948d4fea909dff253c
Lukas Kuprys <lukas@Lukass-Air-2.lan>
2024-12-23 11:57:05 +0200

    update models script

--- scripts/run_models.py
+++ scripts/run_models.py
@@ -3,28 +3,29 @@ import evaluator
 import pandas as pd
 
 ITERATIONS = 10
+FILEPATH = '../outputs/results3.csv'
 
 result = evaluator.evaluate(dataframe = chromatin_algos.algo5d(), model_name="test run")
 
-for i in range(ITERATIONS):
+""" for i in range(ITERATIONS):
     scale = 2 ** i
     print("Runnning k-means, scale", scale)
     evaluation = evaluator.evaluate(dataframe = chromatin_algos.algo5d(scale), model_name=f"k-means 5 features scale={scale}")
     result = pd.concat([result, evaluation], axis=1)
-    result.to_csv('../outputs/results2.csv', sep = '\t')
+    result.to_csv(FILEPATH, sep = '\t')
 
 for i in range(ITERATIONS):
     gamma = 2 ** i
     print("Runnning k-prototypes, gamma", gamma)
     evaluation = evaluator.evaluate(dataframe = chromatin_algos.algo_prototypes(gamma), model_name=f"k-prototypes 5 features gamma={gamma}")
     result = pd.concat([result, evaluation], axis=1)
-    result.to_csv('../outputs/results2.csv', sep = '\t')
+    result.to_csv(FILEPATH, sep = '\t') """
 
 for i in range(ITERATIONS):
-    eps = 0,1 * (2 ** i)
+    eps = 0.1 * (2 ** i)
     print("Runnning DBSCAN, epsilon", eps)
     evaluation = evaluator.evaluate(dataframe = chromatin_algos.algo_dbscan(eps), model_name=f"DBSCAN eps={eps}")
     result = pd.concat([result, evaluation], axis=1)
-    result.to_csv('../outputs/results2.csv', sep = '\t')
+    result.to_csv(FILEPATH, sep = '\t')
 
-result.to_csv('../outputs/results2.csv', sep = '\t')
+result.to_csv(FILEPATH, sep = '\t')
