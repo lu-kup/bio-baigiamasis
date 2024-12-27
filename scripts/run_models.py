37a6e1d4158f37439c78ec10a100890a459e72b6
Lukas Kuprys <lukas@Lukass-Air-2.lan>
2024-12-28 00:01:21 +0200

    fix

--- scripts/run_models.py
+++ scripts/run_models.py
@@ -7,13 +7,13 @@ FILEPATH = '../outputs/results_regression_test.csv'
 
 thresholds = [1, 1.5, 2, 2.5, 3]
 
-result = evaluator.evaluate(dataframe = chromatin_algos.algo5d(), model_name="test run")
+result = evaluator.evaluate(merged_ranges = chromatin_algos.algo5d(), model_name="test run")
 
 for i in range(ITERATIONS):
     for threshold in thresholds:
         eps = 0.0001 * (2 ** i)
         print("Runnning DBSCAN, epsilon", eps)
-        evaluation = evaluator.evaluate(dataframe = chromatin_algos.algo_dbscan_aggregated(eps=eps, threshold=threshold), model_name=f"DBSCAN eps={eps}, threshold={threshold}")
+        evaluation = evaluator.evaluate(merged_ranges = chromatin_algos.algo_dbscan_aggregated(eps=eps, threshold=threshold), model_name=f"DBSCAN eps={eps}, threshold={threshold}")
         result = pd.concat([result, evaluation], axis=1)
         result.to_csv(FILEPATH, sep = '\t')
 
@@ -21,7 +21,7 @@ for i in range(ITERATIONS):
     for threshold in thresholds:
         n_clusters = 2 ** i
         print("Runnning k-means, n-clusters", n_clusters)
-        evaluation = evaluator.evaluate(dataframe = chromatin_algos.algo5d_aggregated(n_clusters=n_clusters, threshold=threshold), model_name=f"k-means 5 features n_clusters={n_clusters}, threshold={threshold}")
+        evaluation = evaluator.evaluate(merged_ranges = chromatin_algos.algo5d_aggregated(n_clusters=n_clusters, threshold=threshold), model_name=f"k-means 5 features n_clusters={n_clusters}, threshold={threshold}")
         result = pd.concat([result, evaluation], axis=1)
         result.to_csv(FILEPATH, sep = '\t')
 
@@ -29,7 +29,7 @@ for i in range(ITERATIONS):
     for threshold in thresholds:
         n_clusters = 2 ** i
         print("Runnning k-prototypes, n-clusters", n_clusters)
-        evaluation = evaluator.evaluate(dataframe = chromatin_algos.algo_prototypes_aggregated(n_clusters=n_clusters, threshold=threshold), model_name=f"k-prototypes 5 features n_clusters={n_clusters}, threshold={threshold}")
+        evaluation = evaluator.evaluate(merged_ranges = chromatin_algos.algo_prototypes_aggregated(n_clusters=n_clusters, threshold=threshold), model_name=f"k-prototypes 5 features n_clusters={n_clusters}, threshold={threshold}")
         result = pd.concat([result, evaluation], axis=1)
         result.to_csv(FILEPATH, sep = '\t')
 
