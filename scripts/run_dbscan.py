bfd7007a72daba12ee58d2b58bfe97c8769f407c
Lukas Kuprys <lukas@Lukass-Air-2.lan>
2024-12-28 00:22:34 +0200

    dbscan script

---
+++ scripts/run_dbscan.py
@@ -0,0 +1,22 @@
+import chromatin_algos
+import evaluator
+import pandas as pd
+import generate_consensus
+
+ITERATIONS = 10
+FILEPATH = '../outputs/results_dbscan.csv'
+
+thresholds = [1, 1.5, 2, 2.5, 3]
+
+result = evaluator.evaluate(merged_ranges = chromatin_algos.algo5d_aggregated(), model_name="test run")
+
+for i in range(ITERATIONS):
+    for threshold in thresholds:
+        eps = 0.0001 * (2 ** i)
+        print("Runnning DBSCAN, epsilon", eps)
+        out = generate_consensus.generate_consensus_5x20(chromatin_algos.algo_dbscan_aggregated, [eps, 6, threshold])
+        evaluation = evaluator.evaluate(merged_ranges = out, model_name=f"DBSCAN eps={eps}, threshold={threshold}")
+        result = pd.concat([result, evaluation], axis=1)
+        result.to_csv(FILEPATH, sep = '\t')
+
+result.to_csv(FILEPATH, sep = '\t')
