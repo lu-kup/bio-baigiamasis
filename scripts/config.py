8fa0bafb1d097383622400410e2903b2db9413d8
Lukas Kuprys <lukas@Lukass-MacBook-Air-2.local>
2025-01-13 13:58:43 +0200

    prepare chr2

--- scripts/config.py
+++ scripts/config.py
@@ -10,9 +10,9 @@ CHR_SIZES_GRCm38 = {
 }
 
 # Chromosome
-CHR = '10'
-SAMPLE_FILEPATH = '../inputs/subset_chr10.rds'
-GENCODE_DATA_PATH = "../inputs/gencode_chr10_M25.gtf"
+CHR = '2'
+SAMPLE_FILEPATH = '../inputs/subset_chr2.rds'
+GENCODE_DATA_PATH = "../inputs/gencode_chr2_M25.gtf"
 
 # Subset size
 CHR_SIZE = CHR_SIZES_GRCm38[CHR]
